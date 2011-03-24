f1236000a84ba3b9cfa2091656a504385f7d9042
benberm <benberm@702bc0d3-552f-47ad-8d22-b90160be019a>
2011-03-24 16:42:05 +0000

    A template to make new cytosine walkers.
    git-svn-id: https://svn.code.sf.net/p/uecgatk/code/trunk@21 702bc0d3-552f-47ad-8d22-b90160be019a

---
+++ src/edu/usc/epigenome/uecgatk/benWalkers/cytosineWalkers/CytosineTEMPLATEWalker.java
@@ -0,0 +1,85 @@
+package edu.usc.epigenome.uecgatk.benWalkers.cytosineWalkers;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.broadinstitute.sting.utils.collections.Pair;
+
+import edu.usc.epigenome.genomeLibs.ListUtils;
+import edu.usc.epigenome.genomeLibs.MethylDb.Cpg;
+import edu.usc.epigenome.genomeLibs.MethylDb.CpgSummarizers.CpgMethLevelSummarizer;
+import edu.usc.epigenome.genomeLibs.MethylDb.CpgSummarizers.CpgMethLevelSummarizer;
+import edu.usc.epigenome.uecgatk.benWalkers.LocusWalkerToBisulfiteCytosineWalker;
+
+/**
+ * @author benb
+ * 
+ */
+public class CytosineTEMPLATEWalker extends LocusWalkerToBisulfiteCytosineWalker<Integer, Long> {
+
+
+
+	/**
+	 * locus walker overrides
+	 */
+
+
+	/**
+	 * Provides an initial value for the reduce function.  Hello walker counts loci,
+	 * so the base case for the inductive step is 0, indicating that the walker has seen 0 loci.
+	 * @return 0.
+	 */
+	@Override
+	public Long reduceInit()
+	{ 
+		Long out = 0L;
+		return out;
+	}
+
+
+
+	/* (non-Javadoc)
+	 * @see org.broadinstitute.sting.gatk.walkers.TreeReducible#treeReduce(java.lang.Object, java.lang.Object)
+	 * 
+	 * Must override this to get multithreaded capability
+	 */
+	@Override
+	public Long treeReduce(Long lhs, Long rhs) 
+	{
+		return lhs + rhs;
+	}
+
+	/**
+	 * Retrieves the final result of the traversal.
+	 * @param result The ultimate value of the traversal, produced when map[n] is combined with reduce[n-1]
+	 *               by the reduce function. 
+	 */
+	@Override
+	public void onTraversalDone(Long result) 
+	{
+		out.printf("Saw %d cytosines\n", result);
+	}
+
+	
+	/***************************************************
+	 * cytosine walker overrides
+	 ***************************************************/
+	
+	@Override
+	protected Integer processCytosine(Cpg thisC)
+	{
+		String context = thisC.context();
+		double meth = thisC.fracMeth(false);
+		return 1;
+	}
+
+
+	@Override
+	protected Long reduceCytosines(Integer value, Long sum) 
+	{
+		return value.longValue() + sum;
+	}
+	
+}
