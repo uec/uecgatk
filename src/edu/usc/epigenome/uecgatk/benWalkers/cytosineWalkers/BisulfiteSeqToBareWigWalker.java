aa8a5e1808a4b50d22f312998279874a6c098621
benberm <benberm@702bc0d3-552f-47ad-8d22-b90160be019a>
2011-04-24 07:25:05 +0000

    Totally changed it to a very simple single-threaded version.  It now works on multiple chromosomes.
    git-svn-id: https://svn.code.sf.net/p/uecgatk/code/trunk@70 702bc0d3-552f-47ad-8d22-b90160be019a

--- src/edu/usc/epigenome/uecgatk/benWalkers/cytosineWalkers/BisulfiteSeqToBareWigWalker.java
+++ src/edu/usc/epigenome/uecgatk/benWalkers/cytosineWalkers/BisulfiteSeqToBareWigWalker.java
@@ -1,32 +1,21 @@
 package edu.usc.epigenome.uecgatk.benWalkers.cytosineWalkers;
 
 import java.io.File;
-import java.io.PrintStream;
-import java.util.Collection;
+import java.io.PrintWriter;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 import org.broadinstitute.sting.commandline.Argument;
-import org.broadinstitute.sting.commandline.Output;
-import org.broadinstitute.sting.utils.wiggle.WiggleHeader;
 
-import edu.usc.epigenome.genomeLibs.MethylDb.CpgSummarizers.CpgMethLevelSummarizer;
 import edu.usc.epigenome.uecgatk.benWalkers.CpgBackedByGatk;
 import edu.usc.epigenome.uecgatk.benWalkers.LocusWalkerToBisulfiteCytosineWalker;
-import edu.usc.epigenome.uecgatk.benWalkers.WiggleHeaderCytosines;
-import edu.usc.epigenome.uecgatk.WiggleWriterReducible;
+
 
 /**
  * @author benb
  * 
  */
-public class BisulfiteSeqToBareWigWalker extends LocusWalkerToBisulfiteCytosineWalker<Map<String,WiggleWriterReducible>, Map<String,WiggleWriterReducible>> {
-
-//    @Output
-//    @Multiplex(value=OutputMultiplexer.class,arguments={"outPrefix"})
-//    Map<String,PrintStream> outStreams;
+public class BisulfiteSeqToBareWigWalker extends LocusWalkerToBisulfiteCytosineWalker<Integer, Long> {
 
 
     @Argument(fullName = "outPrefix", shortName = "pre", doc = "Output prefix for all output files", required = true)
@@ -39,9 +28,9 @@ public class BisulfiteSeqToBareWigWalker extends LocusWalkerToBisulfiteCytosineW
     public boolean outputAllContexts = false;
     
     
-    Map<String,WiggleWriterReducible> wigByContext = new HashMap<String,WiggleWriterReducible>();
-
-
+    Map<String,PrintWriter> wigByContext = new HashMap<String,PrintWriter>();
+    
+    
 	/**
 	 * locus walker overrides
 	 */
@@ -53,17 +42,14 @@ public class BisulfiteSeqToBareWigWalker extends LocusWalkerToBisulfiteCytosineW
 	 * @return 0.
 	 */
 	@Override
-	public Map<String,WiggleWriterReducible> reduceInit()
-	{
-		//Long out = 0L;
-		Map<String,WiggleWriterReducible> out = new HashMap<String,WiggleWriterReducible>();
+	public Long reduceInit()
+	{ 
+		Long out = 0L;
 		return out;
 	}
 
-
 	@Override
 	public void initialize() {
-		// TODO Auto-generated method stub
 		super.initialize();
 		
 		if (this.getToolkit().getArguments().numberOfThreads>1)
@@ -76,6 +62,7 @@ public class BisulfiteSeqToBareWigWalker extends LocusWalkerToBisulfiteCytosineW
 	}
 
 
+	
 
 	/* (non-Javadoc)
 	 * @see org.broadinstitute.sting.gatk.walkers.TreeReducible#treeReduce(java.lang.Object, java.lang.Object)
@@ -83,12 +70,11 @@ public class BisulfiteSeqToBareWigWalker extends LocusWalkerToBisulfiteCytosineW
 	 * Must override this to get multithreaded capability
 	 */
 	@Override
-	public Map<String,WiggleWriterReducible> treeReduce(Map<String,WiggleWriterReducible> a, Map<String,WiggleWriterReducible> b)
+	public Long treeReduce(Long lhs, Long rhs) 
 	{
 		System.err.println("GnomeSeqToBareWigWalker does not yet implement multi-threaded mode. Use -nt 1");
 		System.exit(1);
-		
-		return this.reduceCytosines(a, b);
+		return lhs + rhs;
 	}
 
 	/**
@@ -97,13 +83,14 @@ public class BisulfiteSeqToBareWigWalker extends LocusWalkerToBisulfiteCytosineW
 	 *               by the reduce function. 
 	 */
 	@Override
-	public void onTraversalDone(Map<String,WiggleWriterReducible> result) 
+	public void onTraversalDone(Long result) 
 	{
-//		for (String context : result.keySet())
-//		{
-//			WiggleWriterReducible wig = result.get(context);
-//			
-//		}
+		out.printf("Saw %d cytosines\n", result);
+		for (String context : this.wigByContext.keySet())
+		{
+			PrintWriter pw = this.wigByContext.get(context);
+			pw.close();
+		}
 	}
 
 	
@@ -111,15 +98,26 @@ public class BisulfiteSeqToBareWigWalker extends LocusWalkerToBisulfiteCytosineW
 	 * cytosine walker overrides
 	 ***************************************************/
 	
+	
 	@Override
 	protected void alertNewContig(String newContig) 
 	{
+		System.err.printf("New chrom: %s\n",newContig);
+		
+		for (String context : this.wigByContext.keySet())
+		{
+			PrintWriter pw = this.wigByContext.get(context);
+			pw.printf("variableStep chrom=%s\n", newContig);
+		}
 	}
 
+	
+
 	@Override
-	protected Map<String,WiggleWriterReducible> processCytosine(CpgBackedByGatk thisC)
+	protected Integer processCytosine(CpgBackedByGatk thisC)
 	{
-		writeCoverage(thisC);
+		Integer out = 0;
+		//writeCoverage(thisC);
 		
 		String context = thisC.context();
 		
@@ -131,143 +129,90 @@ public class BisulfiteSeqToBareWigWalker extends LocusWalkerToBisulfiteCytosineW
 		// Skip optional contexts
 		if (!this.outputAllContexts)
 		{
-			if (context.equalsIgnoreCase("HCH") || context.equalsIgnoreCase("GCG")) return this.wigByContext;
+			if (context.equalsIgnoreCase("HCH") || context.equalsIgnoreCase("GCG")) return out;
 		}
 		
 		double meth = thisC.fracMeth(false);
 		
-		WiggleWriterReducible wig = null;
+		PrintWriter wigpw = null;
 		if (this.wigByContext.containsKey(context))
 		{
-			wig = this.wigByContext.get(context);
+			wigpw = this.wigByContext.get(context);
 			//logger.info("Found wig " + wig.toString());
 		}
 		else
 		{
 			// We use the wigByContext for merging
 //			String outfn = String.format("%s.%s.wig.%s",this.outPrefix,context,this.wigByContext.hashCode());
-			String name = String.format("%s.%s", this.outPrefix, context);
+			String name = String.format("%s.%s-minct%d-minconv%d", this.outPrefix, context, this.minCT, this.minConv);
 			String outfn = String.format("%s.wig",name,context);
 			logger.info("NEW wig " + outfn);
-			wig = new WiggleWriterReducible(new File(outfn));
+			try
+			{
+				wigpw = new PrintWriter(new File(outfn));
+			}
+			catch (Exception e)
+			{
+				System.err.printf("Fatal error, BisulfiteSeqToBareWigWalker can't create file: %s\n",outfn);
+				e.printStackTrace();
+				System.exit(1);
+			}
 			
-			WiggleHeader head = new WiggleHeaderCytosines(name, name);
-			wig.writeHeader(head);
-			this.wigByContext.put(context, wig);
+			String desc = String.format("%s-wig", name);
+			wigpw.printf("track type=wiggle_0 name=%s description=%s color=204,102,0 visibility=full " +
+					" graphType=points autoScale=off alwaysZero=off maxHeightPixels=64:32:10 viewLimits=0:100\n", desc, desc);	
+			wigpw.printf("variableStep chrom=%s\n", thisC.getGenomeLoc().getContig());
+
+			this.wigByContext.put(context, wigpw);
 		}
 		
-		wig.writeData(thisC.getGenomeLoc(), Math.round(100.0*meth));
+		wigpw.printf("%d\t%d\n",thisC.getGenomeLoc().getStart(), (int)Math.round(100.0*meth));
 		//logger.info(String.format("\tWriting pos %d to wig \"%s\" (this=%s)\n", thisC.chromPos, context,this));
+		out++;
 		
-		return this.wigByContext;
+		return out;
 	}
 
-
-	private void writeCoverage(CpgBackedByGatk thisC)
-	{
-		String context = "readcvg";
-		WiggleWriterReducible wig = null;
-		if (this.wigByContext.containsKey(context))
-		{
-			wig = this.wigByContext.get(context);
-			//logger.info("Found wig " + wig.toString());
-		}
-		else
-		{
-			// We use the wigByContext for merging
-//			String outfn = String.format("%s.%s.wig.%s",this.outPrefix,context,this.wigByContext.hashCode());
-			String name = String.format("%s.%s", this.outPrefix, context);
-			String outfn = String.format("%s.wig",name,context);
-			logger.info("NEW wig " + outfn);
-			wig = new WiggleWriterReducible(new File(outfn));
-			
-			WiggleHeader head = new WiggleHeaderCytosines(name, name);
-			wig.writeHeader(head);
-			this.wigByContext.put(context, wig);
-		}
-		
-		wig.writeData(thisC.getGenomeLoc(), thisC.totalReads);
-	}
 
 
 	@Override
-	protected Map<String,WiggleWriterReducible> reduceCytosines(Map<String,WiggleWriterReducible> newMap, Map<String,WiggleWriterReducible> oldMap) 
+	protected Long reduceCytosines(Integer value, Long sum) 
 	{
-		Map<String,WiggleWriterReducible> outMap = null;
-		
-		if (newMap.hashCode() == oldMap.hashCode())
-		{
-			// Do nothing, same object
-			outMap = oldMap;
-		}
-		else
-		{
-			logger.info(String.format("Actually combining maps: (%s,%s)\n",newMap.hashCode(),oldMap.hashCode()));
-			outMap = this.actuallyReduceCytosines(newMap, oldMap);
-		}
-		
-		return outMap;
+		return value.longValue() + sum;
 	}
+
 	
-	private Map<String,WiggleWriterReducible> actuallyReduceCytosines(Map<String,WiggleWriterReducible> newMap, Map<String,WiggleWriterReducible> oldMap) 
-	{
-		Map<String,WiggleWriterReducible> outmap = oldMap; //new HashMap<String,WiggleWriterReducible>();
-		
-		Set<String> keys = new HashSet<String>();
-		keys.addAll(newMap.keySet());
-		keys.addAll(oldMap.keySet());
-		for (String key : keys)
-		{
-			if (newMap.containsKey(key) && !oldMap.containsKey(key))
-			{
-				outmap.put(key, newMap.get(key));
-			}
-			else if (!newMap.containsKey(key) && oldMap.containsKey(key))
-			{
-				outmap.put(key, oldMap.get(key));
-			}
-			else if (!newMap.containsKey(key) && !oldMap.containsKey(key))
-			{
-				// SHOULD NOT BE HERE
-				logger.error(String.format("GnomeSeqToBareWigWalker::actuallyReduceCytosines() - How did we get key \"%s\" when neither lhs and rhs contain it??",key));
-				System.exit(1);
-			}
-			else
-			{
-				// They must both contain the key.  Merge
-				//logger.info(String.format("About to combine a and b WigWriters for key \"%s\"",key)); 
-				String outfn = String.format("%s.%s.wig.%s",this.outPrefix,key,this.hashCode());
-				WiggleWriterReducible combined = WiggleWriterReducible.merge(newMap.get(key), oldMap.get(key), new File(outfn));
-				outmap.put(key, combined);
-			}		
-		}
-			
-		return outmap;
-	}
-	
+	//////////////////////
+	/// Private funcs
+	//////////////////////
+
+//	private void writeCoverage(CpgBackedByGatk thisC)
+//	{
+//		String context = "readcvg";
+//		WiggleWriterReducible wig = null;
+//		if (this.wigByContext.containsKey(context))
+//		{
+//			wig = this.wigByContext.get(context);
+//			//logger.info("Found wig " + wig.toString());
+//		}
+//		else
+//		{
+//			// We use the wigByContext for merging
+////			String outfn = String.format("%s.%s.wig.%s",this.outPrefix,context,this.wigByContext.hashCode());
+//			String name = String.format("%s.%s", this.outPrefix, context);
+//			String outfn = String.format("%s.wig",name,context);
+//			logger.info("NEW wig " + outfn);
+//			wig = new WiggleWriterReducible(new File(outfn));
+//			
+//			WiggleHeader head = new WiggleHeaderCytosines(name, name);
+//			wig.writeHeader(head);
+//			this.wigByContext.put(context, wig);
+//		}
+//		
+//		wig.writeData(thisC.getGenomeLoc(), thisC.totalReads);
+//	}
+
 }
-
-
-//class OutputMultiplexer implements Multiplexer<String>
-//{
-//
-//	private String prefix = "prefix";
-//	
-//	public OutputMultiplexer(String inPrefix) {
-//		super();
-//		this.prefix = inPrefix;
-//	}
-//
-//	@Override
-//	public Collection<String> multiplex() {
-//		return null;
-//	}
-//
-//	@Override
-//	public String transformArgument(String multiplexedEntry, String argument) {
-//		return null;
-//	}
-//}
 
 
 
