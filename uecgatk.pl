63a9f5a2c936a9380b1fdfc4833712e1a74ab7dd
zack ramjan <zack.hr@gmail.com>
2014-01-15 15:28:56 -0800

    added filter for NH != 1

--- uecgatk.pl
+++ uecgatk.pl
@@ -3,7 +3,7 @@
 $cmd = join ' ', @ARGV;
 
 #java to use
-$JAVA = "/home/uec-00/shared/production/software/java/default/bin/java -Xmx7G";
+$JAVA = "/home/uec-00/shared/production/software/java/default_java7/bin/java -Xmx7G";
 $libpath = "/home/uec-00/shared/production/software/uecgatk2/2014-01-15/lib";
 $uecgatk = "/home/uec-00/shared/production/software/uecgatk2/2014-01-15/uecgatk.jar";
 
@@ -11,6 +11,6 @@ $uecgatk = "/home/uec-00/shared/production/software/uecgatk2/2014-01-15/uecgatk.
 $JARLIBS = "$libpath/GenomeAnalysisTK-2.7-4-g6f46d11.jar:$libpath/batik-anim.jar:$libpath/batik-css.jar:$libpath/batik-dom.jar:$libpath/batik-ext.jar:$libpath/batik-parser.jar:$libpath/batik-svg-dom.jar:$libpath/batik-util.jar:$libpath/batik-xml.jar:$libpath/biojava-live.jar:$libpath/charts4j-1.2.jar:$libpath/commons-math3-3.2.jar:$libpath/heatMap.jar:$libpath/sam-1.61.jar:$libpath/tuple.jar:$libpath/UscKeck.jar:$libpath/xml-apis-ext.jar:$libpath/xml-apis.jar:$libpath/genomeLibs.jar:$uecgatk";
 
 
-$execString = "$JAVA -classpath $JARLIBS org.broadinstitute.sting.gatk.CommandLineGATK $cmd ";
+$execString = "$JAVA -classpath $JARLIBS org.broadinstitute.sting.gatk.CommandLineGATK $cmd -U ALLOW_N_CIGAR_READS";
 print "$execString\n";
 system($execString);
